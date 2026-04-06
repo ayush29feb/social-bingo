79973420e5ae1a6bbd01fb770ff32db90d7bccec
Ayush Saraf <ayush29feb@Ayushs-MacBook-Air.local>
2026-04-05 21:26:52 -0400

    feat: add app color palette

---
+++ SwiftApp/SocialBingo/Constants/Colors.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+    static let appPrimary     = Color(red: 108/255, green:  99/255, blue: 255/255)
+    static let appPrimaryLight = Color(red: 238/255, green: 240/255, blue: 255/255)
+    static let appBackground  = Color(red: 245/255, green: 245/255, blue: 245/255)
+    static let appText        = Color(red:  26/255, green:  26/255, blue:  26/255)
+    static let appMuted       = Color(red: 136/255, green: 136/255, blue: 136/255)
+    static let appBorder      = Color(red: 224/255, green: 224/255, blue: 224/255)
+    static let appPlusOne     = Color(red: 255/255, green: 107/255, blue: 107/255)
+    static let appPlusOneBg   = Color(red: 255/255, green: 240/255, blue: 240/255)
+}
